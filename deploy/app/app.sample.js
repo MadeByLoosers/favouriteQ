322ff993bc1866e6aee7b29ec1e5c7108bbb9fd3
Pete Goodman <thegingerbloke@gmail.com>
2012-04-28 15:55:41 +0100

    now connecting to twitter...

---
+++ deploy/app/app.sample.js
@@ -0,0 +1,12 @@
+/*
+ * Start favouriteQ
+ */
+var favouriteQ = require("./favouriteQ");
+    
+favouriteQ.init({
+  twitterAccount          : "favouriteQ",
+  consumer_key            : "x",
+  consumer_secret         : "x",
+  access_token_key        : "x",
+  access_token_secret     : "x"
+});
