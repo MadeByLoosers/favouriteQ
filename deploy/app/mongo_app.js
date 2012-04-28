25e8bf10e6419929592e116019e870612bfae261
Pete Graham <petegraham1@gmail.com>
2012-04-28 15:33:10 +0100

    answers returning?

--- deploy/app/mongo_app.js
+++ deploy/app/mongo_app.js
@@ -5,53 +5,43 @@ var db = require("mongojs").connect(databaseUrl, collections);
 
 // get active question from mongo
 function getActiveQuestionId(callback){
-  var activeId = false;
   db.questions.find({active: true}, function(err, questions) {
     if( err || !questions) console.log("No questions found");
     
     questions.forEach( function(question) {
       callback(question._id);
-      //console.log(question._id);
-      //return question._id;
+      //getAnswersForQuestion(question._id);
     });
   });
 }
 
+/*
+function getAnswersForQuestion(questionId){
+  console.log('got answers for ' + questionId);
+}
+*/
+//function add Answer() do we need add user?
+
+
+//getActiveQuestionId();
+/*
 getActiveQuestionId(function(id){
-  console.log('in callback');
-  console.log("active question id " + id);
-  // 4. Get answers for active question (order)
+  console.log('got answers for ' + id);
 });
+*/
+getActiveQuestionId(function getAnswersForQuestion(questionId){
+  console.log('got answers for ' + questionId);
+  // Order by needed?
+  
+  db.answers.find({question_id: questionId}, function(err, answers) {
+    if( err || !answers) console.log("No answers found");
+    else console.log("answers mofo!");
+
+    answers.forEach( function(answer) {
+      console.log(answer);
+    });
+  });
+});
+//getActiveQuestionId(getAnswersForQuestion(id));
 
 // 5. Dump DB?
-
-/*
-// Save Questions
-db.questions.save({question: "Who's your favourite Jew?", asked: false});
-db.questions.save({question: "What's your favourite cheese?", asked: false});
-db.questions.save({question: "What's your favourite cephalopod?", asked: false});
-/*, function(err, saved) {
-  if( err || !saved ) console.log("question not saved");
-  else console.log("question saved");
-});
-*/
-
-/*
-// 3. update data
-// app.js
-db.users.update({email: "srirangan@gmail.com"}, {$set: {password: "iReallyLoveJews"}}, function(err, updated) {
-  if( err || !updated ) console.log("User not updated");
-  else console.log("User updated");
-});
-*/
-
-/*
-// 2. look up questions (select)
-//db.questions.find({sex: "male"}, function(err, users) {
-db.questions.find({}, function(err, users) {
-  if( err || !users) console.log("No questions found");
-  else users.forEach( function(maleUser) {
-    console.log(maleUser);
-  } );
-});
-*/
