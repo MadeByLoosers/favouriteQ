82199d4fa75b28cb90a72d2d5c76c24f5db01280
Pete Graham <petegraham1@gmail.com>
2012-08-25 18:35:42 +0100

    Question and answers displaying

--- deploy/favouriteQ/questions/views.py
+++ deploy/favouriteQ/questions/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render_to_response
+from questions.models import Question, Answer
 
 def current_question(request):
-    # select a random poll
-    #p = Poll.objects.order_by('?')[0]
-    # redirect to voting page for that poll id
-    #print 'test' + (str)p.id
-    #return HttpResponseRedirect(reverse('poll_detail', args=(p.id,)))
-    return render_to_response('questions/current_question.html')
+
+    # get the question with the most recent asked date (null asked date means question hasn't been asked)
+    question = Question.objects.order_by('-asked_date').filter(asked_date__isnull=False)[0]
+    #print q
+    # get answers for the question
+
+    return render_to_response('questions/current_question.html', {"question": question})
     # {"results": results, "query": query}, context_instance=RequestContext(request)
