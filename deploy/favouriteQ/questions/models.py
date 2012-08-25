de9486f6c111ecbdc208bcd7423a2184808275c5
Pete Graham <petegraham1@gmail.com>
2012-08-25 17:26:05 +0100

    Models and updated DB dump

--- deploy/favouriteQ/questions/models.py
+++ deploy/favouriteQ/questions/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=140)
+    asked_date = models.DateTimeField(null=True) # Null if question hasn't been asked
+    priority = models.BooleanField() # deafult to not priority
+    # automatic system fields
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class Person(models.Model):
+    twitter_username = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=25)
+    surname = models.CharField(max_length=25)
+    middle_names = models.CharField(max_length=25)
+    # automatic system fields
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    answer_text = models.CharField(max_length=140) # should it be less as will include @favouriteQ? possibly allow non-twitter answering in future
+    person = models.ForeignKey(Person)
+    # automatic system fields
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
