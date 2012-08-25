a4969559632e7ebf294cdca2379656a18de4d736
Pete Graham <petegraham1@gmail.com>
2012-08-25 18:52:00 +0100

    Extra db lookup removed

--- deploy/favouriteQ/questions/views.py
+++ deploy/favouriteQ/questions/views.py
@@ -7,9 +7,4 @@ def current_question(request):
     question = Question.objects.order_by('-asked_date').filter(asked_date__isnull=False)[0]
     answers = Answer.objects.filter(question_id=question.id)
 
-    for answer in answers:
-        print answer.answer_text
-        print answer.person
-
     return render_to_response('questions/current_question.html', {"question": question, "answers":answers})
-    # {"results": results, "query": query}, context_instance=RequestContext(request)
