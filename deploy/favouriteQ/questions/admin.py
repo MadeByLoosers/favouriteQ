2dd3251688e5207b8f31a6bef9afce4039033846
Pete Graham <petegraham1@gmail.com>
2012-08-25 17:52:11 +0100

    Test data added in

--- deploy/favouriteQ/questions/admin.py
+++ deploy/favouriteQ/questions/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from questions.models import Question
+from questions.models import Question, Person, Answer
 
 admin.site.register(Question)
+admin.site.register(Person)
+admin.site.register(Answer)
--- deploy/favouriteQ/questions/models.py
+++ deploy/favouriteQ/questions/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Question(models.Model):
     question = models.CharField(max_length=140)
-    asked_date = models.DateTimeField(null=True) # Null if question hasn't been asked
+    asked_date = models.DateTimeField(null=True, blank=True) # Null if question hasn't been asked
     priority = models.BooleanField() # deafult to not priority
     # automatic system fields
     created_at = models.DateTimeField(auto_now_add = True)
@@ -15,13 +15,16 @@ class Person(models.Model):
     twitter_username = models.CharField(max_length=20)
     first_name = models.CharField(max_length=25)
     surname = models.CharField(max_length=25)
-    middle_names = models.CharField(max_length=25)
+    middle_names = models.CharField(max_length=25, blank=True)
     # automatic system fields
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
     def __unicode__(self):
         return self.twitter_username
+
+    class Meta:
+        verbose_name_plural = "People"
 
 class Answer(models.Model):
     question = models.ForeignKey(Question)
