658ad2ffa6e1c81f8edfca8ef27292f5041ed4f5
Pete Graham <petegraham1@gmail.com>
2012-08-26 12:56:27 +0100

    not required variables removed

--- deploy/favouriteQ/questions/views.py
+++ deploy/favouriteQ/questions/views.py
@@ -5,10 +5,7 @@ from questions.models import Question, Answer
 def current_question(request):
     # get the question with the most recent asked date (null asked date means question hasn't been asked)
     question = Question.objects.order_by('-asked_date').filter(asked_date__isnull=False)[0]
-
     return render_to_response('questions/current_question.html', {"question": question}, context_instance=RequestContext(request))
 
 def suggest_question(request):
-    # get the question with the most recent asked date (null asked date means question hasn't been asked)
-    question = Question.objects.order_by('-asked_date').filter(asked_date__isnull=False)[0]
-    return render_to_response('questions/suggest_question.html', {"question": question}, context_instance=RequestContext(request))
+    return render_to_response('questions/suggest_question.html', context_instance=RequestContext(request))
