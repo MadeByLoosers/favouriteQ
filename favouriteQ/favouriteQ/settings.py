6fc92acd77c78d82253169d39476fd8e6f4b53e9
Pete X. Graham <pete.graham@piratalondon.com>
2012-08-29 17:55:29 +0100

    twitter api settings moved to settings.py

--- favouriteQ/favouriteQ/settings.py
+++ favouriteQ/favouriteQ/settings.py
@@ -153,6 +153,13 @@ LOGGING = {
     }
 }
 
+TWITTER_API = {
+    'consumer_key': 'X6GT49dzDNiePLCaNIHiAg',
+    'consumer_secret': '4kV5v2DXFUdfYJpa8HalgATLK20zmtU1ZQeWzatw',
+    'access_token_key': '565461060-bnetbvYCncn824yxgamZTbVeJHDu66UKDquFrv0A',
+    'access_token_secret': 'ZjNQwRQVbI9RkDXvlQkrAEEbqECIkPZgtwiSvWFS0ok'
+}
+
 try:
     from localsettings.localsettings import *
 except:
--- favouriteQ/post_to_twitter.py
+++ favouriteQ/post_to_twitter.py
@@ -5,16 +5,17 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "favouriteQ.settings")
 from django.core.management import execute_from_command_line
 from questions.models import Question, Answer, Person
 from django.utils import timezone
+from django.conf import settings
 
-#TODO: can this be added to the settings file?
-twitter_api = twitter.Api(consumer_key='X6GT49dzDNiePLCaNIHiAg',
-              consumer_secret='4kV5v2DXFUdfYJpa8HalgATLK20zmtU1ZQeWzatw',
-              access_token_key='565461060-bnetbvYCncn824yxgamZTbVeJHDu66UKDquFrv0A',
-              access_token_secret='ZjNQwRQVbI9RkDXvlQkrAEEbqECIkPZgtwiSvWFS0ok')
+
+twitter_api = twitter.Api(consumer_key=settings.TWITTER_API['consumer_key'],
+              consumer_secret=settings.TWITTER_API['consumer_secret'],
+              access_token_key=settings.TWITTER_API['access_token_key'],
+              access_token_secret=settings.TWITTER_API['access_token_secret'])
 
 question = Question.objects.get_new_question()
 #TODO: add try catch here (in case of API failure)
-status = twitter_api.PostUpdate(question.question) #'test python message')
-#print status.text
+status = twitter_api.PostUpdate(question.question)
+print status.text
 question.asked_date = timezone.now()
 question.save()
