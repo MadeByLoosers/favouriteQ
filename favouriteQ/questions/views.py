c734497a6ac2985d003d1fbd3c1c805dd8234930
Pete X. Graham <pete.graham@piratalondon.com>
2012-08-30 16:32:18 +0100

    Forms WIP

--- favouriteQ/questions/views.py
+++ favouriteQ/questions/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from forms import QuestionForm
 from questions.models import Question, Answer
 
 def current_question(request):
@@ -7,4 +8,12 @@ def current_question(request):
     return render_to_response('questions/current_question.html', {"question": question}, context_instance=RequestContext(request))
 
 def suggest_question(request):
-    return render_to_response('questions/suggest_question.html', context_instance=RequestContext(request))
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            #q = Question(question=form.cleaned_data['question'], pub_date=timezone.now())
+            #q.save()
+            return HttpResponseRedirect(reverse('question_suggested'))
+    else:
+        form = QuestionForm()
+    return render_to_response('questions/suggest_question.html', {'form': form}, context_instance=RequestContext(request))
