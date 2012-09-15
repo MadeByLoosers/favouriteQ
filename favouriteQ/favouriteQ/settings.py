e6fd8df8863fb89a8eeba00aaa8ce690157dea39
Pete Graham <petegraham1@gmail.com>
2012-09-15 14:56:00 +0100

    Twitter API details removed

--- favouriteQ/favouriteQ/settings.py
+++ favouriteQ/favouriteQ/settings.py
@@ -153,10 +153,10 @@ LOGGING = {
 }
 
 TWITTER_API = {
-    'consumer_key': 'X6GT49dzDNiePLCaNIHiAg',
-    'consumer_secret': '4kV5v2DXFUdfYJpa8HalgATLK20zmtU1ZQeWzatw',
-    'access_token_key': '565461060-bnetbvYCncn824yxgamZTbVeJHDu66UKDquFrv0A',
-    'access_token_secret': 'ZjNQwRQVbI9RkDXvlQkrAEEbqECIkPZgtwiSvWFS0ok'
+    'consumer_key': '',
+    'consumer_secret': '',
+    'access_token_key': '',
+    'access_token_secret': ''
 }
 
 try:
