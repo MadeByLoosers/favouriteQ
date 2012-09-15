fadf1a4ebc05fd7fc04aea20bca421b620cfeb3f
Pete Goodman <thegingerbloke@gmail.com>
2012-09-15 13:17:20 +0100

    tweaked forms to fix lint errors

--- favouriteQ/questions/forms.py
+++ favouriteQ/questions/forms.py
@@ -1,15 +1,16 @@
 from django import forms
 
+
 class QuestionForm(forms.Form):
-    question = forms.CharField(widget=forms.Textarea(attrs={'rows':'5',
-                                                            'cols': '5', 
+    question = forms.CharField(widget=forms.Textarea(attrs={'rows': '5',
+                                                            'cols': '5',
                                                             'placeholder': 'What is your favourite...',
                                                             'maxlength': '130',
                                                             'required': 'required'
                                                     }), max_length=130, required=True, label='What is your favourite...')
 
-    twitter_user = forms.CharField(widget=forms.Textarea(attrs={'rows':'1',
-                                                                'cols': '25', 
+    twitter_user = forms.CharField(widget=forms.Textarea(attrs={'rows': '1',
+                                                                'cols': '25',
                                                                 'placeholder': '@your_twitter_handle (optional)',
                                                                 'maxlength': '20',
                                                     }), max_length=20, required=False, label='@your_twitter_handle (optional)')
