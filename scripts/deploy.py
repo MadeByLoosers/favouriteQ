a85466d3ba82e396e7a164f245ac252030bc392d
Pete Graham <petegraham1@gmail.com>
2012-09-16 14:14:19 +0100

    Rsync updated to not delete the settings file

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,13 +5,13 @@ import sys
 (status, output) = commands.getstatusoutput('git pull')
 print output
 
-cmd = "rsync -av --delete --exclude .git* /srv/www/git_favouriteQ/ /srv/www/www.favouritequestion.com/"
+cmd = "rsync -av --delete --exclude .git* --exclude localsettings.py /srv/www/git_favouriteQ/ /srv/www/www.favouritequestion.com/"
 print cmd
 (status, output) = commands.getstatusoutput(cmd)
-if status:    ## Error case, print the command's output to stderr and exit
+if status:    # Error case, print the command's output to stderr and exit
     sys.stderr.write(output)
     sys.exit(1)
-print output  ## Otherwise do something with the command's output
+print output  # Otherwise do something with the command's output
 
 (status, output) = commands.getstatusoutput('python /srv/www/www.favouritequestion.com/favouriteQ/manage.py collectstatic --noinput')
 print output
