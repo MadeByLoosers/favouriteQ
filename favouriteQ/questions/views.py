13b44e5257dd278e6ae714669662be10a0232aab
Pete X. Graham <pete.graham@piratalondon.com>
2012-09-18 15:05:02 +0100

    render function added

--- favouriteQ/questions/views.py
+++ favouriteQ/questions/views.py
@@ -10,7 +10,8 @@ def current_question(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
-            q = Question(question=form.cleaned_data['question'], approved=False)
+            q = Question(question=form.cleaned_data['question'],
+                         approved=False)
             q.save()
         else:
             # shouldn't be reached accept if HTML5 validation is bypassed
@@ -19,19 +20,28 @@ def current_question(request):
         form = QuestionForm()
 
     question = Question.objects.get_current_question()
-    return render_to_response(
-        'questions/current_question.html',
-        {"question": question, "form": form, "current": True, "twitter_account": settings.TWITTER_USER},
-        context_instance=RequestContext(request))
+    params = {
+        "question": question,
+        "form": form,
+        "current": True,
+        "twitter_account": settings.TWITTER_USER
+    }
+    return render_template(request, "questions/current_question.html", params)
 
 
 def archive(request):
     form = QuestionForm()
     questions = Question.objects.order_by('-asked_date').filter(asked_date__isnull=False)
-    return render_to_response(
-        'questions/archive.html',
-        {"questions": questions, "form": form, "twitter_account": settings.TWITTER_USER},
-        context_instance=RequestContext(request))
+    params = {
+        "questions": questions,
+        "form": form,
+        "twitter_account": settings.TWITTER_USER
+    }
+    return render_template(request, "questions/archive.html", params)
+    # return render_to_response(
+    #     'questions/archive.html',
+    #     {"questions": questions, "form": form, "twitter_account": settings.TWITTER_USER},
+    #     context_instance=RequestContext(request))
 
 
 def detail(request, question_id):
@@ -39,7 +49,8 @@ def detail(request, question_id):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
-            q = Question(question=form.cleaned_data['question'], approved=False)
+            q = Question(question=form.cleaned_data['question'],
+                         approved=False)
             q.save()
         else:
             # shouldn't be reached accept if HTML5 validation is bypassed
@@ -49,5 +60,11 @@ def detail(request, question_id):
 
     question = get_object_or_404(Question, Q(asked_date__isnull=False), pk=question_id)  # check it's been asked here
     return render_to_response('questions/current_question.html',
-        {"question": question, "form": form, "twitter_account": settings.TWITTER_USER},
+                              {"question": question, "form": form,
+                              "twitter_account": settings.TWITTER_USER},
         context_instance=RequestContext(request))
+
+
+def render_template(request, template, params):
+    return render_to_response(template, params,
+                              context_instance=RequestContext(request))
