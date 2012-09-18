d0ca3f614a338e7f3e4b1fe91c1b978910698f68
Pete X. Graham <pete.graham@piratalondon.com>
2012-09-18 15:17:52 +0100

    Some duplication removed from views

--- favouriteQ/questions/views.py
+++ favouriteQ/questions/views.py
@@ -7,24 +7,12 @@ from django.conf import settings
 
 
 def current_question(request):
-    if request.method == 'POST':
-        form = QuestionForm(request.POST)
-        if form.is_valid():
-            q = Question(question=form.cleaned_data['question'],
-                         approved=False)
-            q.save()
-        else:
-            # shouldn't be reached accept if HTML5 validation is bypassed
-            print "form not valid"
-    else:
-        form = QuestionForm()
-
+    form = handle_form(request)
     question = Question.objects.get_current_question()
     params = {
         "question": question,
         "form": form,
         "current": True,
-        "twitter_account": settings.TWITTER_USER
     }
     return render_template(request, "questions/current_question.html", params)
 
@@ -35,17 +23,24 @@ def archive(request):
     params = {
         "questions": questions,
         "form": form,
-        "twitter_account": settings.TWITTER_USER
     }
     return render_template(request, "questions/archive.html", params)
-    # return render_to_response(
-    #     'questions/archive.html',
-    #     {"questions": questions, "form": form, "twitter_account": settings.TWITTER_USER},
-    #     context_instance=RequestContext(request))
 
 
 def detail(request, question_id):
-    # Duplication. How to have current_question save this code
+    form = handle_form(request)
+    # check if question has been asked
+    question = get_object_or_404(Question,
+                                 Q(asked_date__isnull=False),
+                                 pk=question_id)
+    params = {
+        "question": question,
+        "form": form,
+    }
+    return render_template(request, "questions/current_question.html", params)
+
+
+def handle_form(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
@@ -57,14 +52,10 @@ def detail(request, question_id):
             print "form not valid"
     else:
         form = QuestionForm()
-
-    question = get_object_or_404(Question, Q(asked_date__isnull=False), pk=question_id)  # check it's been asked here
-    return render_to_response('questions/current_question.html',
-                              {"question": question, "form": form,
-                              "twitter_account": settings.TWITTER_USER},
-        context_instance=RequestContext(request))
+    return form
 
 
 def render_template(request, template, params):
+    params['twitter_account'] = settings.TWITTER_USER
     return render_to_response(template, params,
                               context_instance=RequestContext(request))
