ec2910d3d476c0f3968ca8248848528c934acf87
Pete X. Graham <pete.graham@piratalondon.com>
2012-09-19 16:36:50 +0100

    Lazy attribute used instead of property for number

--- favouriteQ/questions/models.py
+++ favouriteQ/questions/models.py
@@ -1,25 +1,36 @@
 from django.db import models
+from lazy import lazy
 
 
 class QuestionManager(models.Manager):
     def get_current_question(self):
-        # get the question with the most recent asked date (null asked date means question hasn't been asked)
-        return self.order_by('-asked_date').filter(asked_date__isnull=False, approved=1)[0]
+        """
+        Get the question with the most recent asked date
+        (null asked date means question hasn't been asked)
+        """
+        return self.order_by('-asked_date').filter(asked_date__isnull=False,
+                                                   approved=1)[0]
 
     def get_new_question(self):
         # get priority questions first
-        questions = self.order_by('?').filter(asked_date__isnull=True, priority=1, approved=1)
+        questions = self.order_by('?').filter(asked_date__isnull=True,
+                                              priority=1,
+                                              approved=1)
         # no priority questions then get from non-priority
         if not questions:
-            questions = self.order_by('?').filter(asked_date__isnull=True, priority=0, approved=1)
+            questions = self.order_by('?').filter(asked_date__isnull=True,
+                                                  priority=0,
+                                                  approved=1)
         #TODO: add check and behaviour if we don't have a question.
         return questions[0]
 
 
 class Question(models.Model):
     question = models.CharField(max_length=140)
-    asked_date = models.DateTimeField(null=True, blank=True)  # Null if question hasn't been asked
-    priority = models.BooleanField()  # deafult to not priority
+    # Null if question hasn't been asked
+    asked_date = models.DateTimeField(null=True, blank=True)
+    # deafult to not priority
+    priority = models.BooleanField()
     approved = models.BooleanField()
 
     # System fields
@@ -30,9 +41,9 @@ class Question(models.Model):
     def __unicode__(self):
         return self.question
 
-    @property
+    @lazy
     def num(self):
-        count = Question.objects.filter(asked_date__lte=self.asked_date).count()  # TODO: add filter here on asked_date
+        count = Question.objects.filter(asked_date__lte=self.asked_date).count()
         return count
 
 
