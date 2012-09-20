6f80b8c330df4904bbf15830b8a46febae0ce685
Pete X. Graham <pete.graham@piratalondon.com>
2012-09-20 15:01:34 +0100

    Issues with venv

--- favouriteQ/fabfile.py
+++ favouriteQ/fabfile.py
@@ -1,7 +1,7 @@
 #import os
 from fabric.api import env, sudo, local
 from fabric.operations import put, run
-from fabric.context_managers import cd
+from fabric.context_managers import cd, prefix
 
 #TODO: move to environments (may be different for QA, staging, production
 GIT_REPO_PATH = '/srv/www/git_favouriteQ/'
@@ -10,16 +10,17 @@ GIT_REPO_PATH = '/srv/www/git_favouriteQ/'
 def production():
     env.hosts = ['54.245.116.229']
     env.user = 'ec2-user'
-    env.directory = '/srv/www/www.favouritequestion.com'
-    env.activate = 'source /home/ec2-user/virtualenv/favouriteQ/env/bin/activate'
+    env.directory = '/srv/www/www.favouritequestion.com/'
+    env.activate = 'source /home/ec2-user/virtualenv/favouriteQ/env/bin/activate/'
     env.key_filename = ['~/.ssh/django.pem']
 
 
 def staging():
     env.hosts = ['54.245.116.229']
     env.user = 'ec2-user'
-    env.directory = '/srv/www/staging.favouritequestion.com'
-    env.activate = 'source /home/ec2-user/virtualenv/favouriteQ/env/bin/activate'
+    env.directory = '/srv/www/staging.favouritequestion.com/'
+    #TODO: give staging it's own venv
+    env.activate = 'source /home/ec2-user/virtualenv/favouriteQ/env/bin/activate/'
     env.key_filename = ['~/.ssh/django.pem']
 
 
@@ -31,10 +32,6 @@ def local():
     env.virtualhost_path = "/"
 
 
-def mkfile():
-    run('touch ' + env.user + '.txt')
-
-
 def deploy():
     # 1. activate virtual env? Is this neeeded? Done in server setup?
 
@@ -42,19 +39,37 @@ def deploy():
     git_pull()
 
     # 3. run rsync script
-    #require('fab_hosts', provided_by=[production])
-    with cd(env.directory):
-        #mkfile()
-        run('rsync -av --delete --exclude .git* --exclude localsettings.py ' + GIT_REPO_PATH + ' ' + env.directory)
+    rsync_files()
+    # TODO: research symlinking different versions of the site
 
     # 4. run the DB migrations (how to stop this using fixtures on live?)
     # ./manage.py migrate projects
 
     # 5. collect the static files
-    # python /srv/www/www.favouritequestion.com/favouriteQ/manage.py collectstatic --noinput
+    # with prefix('workon ' + env.activate):
+    #     run('which python')
+    #with cd(env.directory):
+    #run('./manage.py collectstatic --noinput
 
     # 6. Restart server
     restart_webserver()
+
+
+def git_pull():
+    with cd(GIT_REPO_PATH):
+        run('git pull')
+
+
+def rsync_files():
+    run('rsync -av --delete --exclude .git* --exclude localsettings.py ' + GIT_REPO_PATH + ' ' + env.directory)
+
+
+def collect_static():
+    #TODO: use os.path.join(,,,
+    manage_py_path = env.directory + 'favouriteQ/manage.py'
+    with prefix('source ' + env.activate):
+        run(manage_py_path)
+    #run('/srv/www/staging.favouritequestion.com/favouriteQ/manage.py collectstatic --noinput')
 
 
 def restart_webserver():
@@ -62,12 +77,6 @@ def restart_webserver():
     #TODO: move name off the app to a variable
     sudo("supervisorctl restart favourite_q")
 
-
-def git_pull():
-    """Updates the repository."""
-    with cd(GIT_REPO_PATH):
-        run('git pull')
-    #with cd(env.directory):
 
 # def syncdb():
 #     with cd(SERVER_PATH):
