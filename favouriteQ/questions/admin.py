2c2c7b9ee2e3c735790809e812cf59db45ce9617
Pete Goodman <thegingerbloke@gmail.com>
2013-03-05 07:32:04 +0000

    added priority column to questions admin view

--- favouriteQ/questions/admin.py
+++ favouriteQ/questions/admin.py
@@ -4,7 +4,7 @@ from questions.models import Question, Person, Answer
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question', 'asked_date', 'approved')
+    list_display = ('question', 'asked_date', 'approved', 'priority')
     list_filter = ['asked_date', 'approved']
     search_fields = ['question']
     ordering = ['asked_date', 'approved']
