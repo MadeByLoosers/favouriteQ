daa0c87b2dbd8d53abd6618c82e169c036782dcd
Pete Graham <petegraham1@gmail.com>
2013-05-03 17:56:18 +0100

    140 char check added

--- favouriteQ/questions/management/commands/archive_current_and_post_new_question.py
+++ favouriteQ/questions/management/commands/archive_current_and_post_new_question.py
@@ -23,9 +23,14 @@ class Command(BaseCommand):
 
         if num_answers > 0:
             # send summary tweet with archive link
-            archive_url = reverse('questions.views.detail', args=(current_question.slug,))
+            archive_url = reverse('questions.views.detail',
+                                  args=(current_question.slug,))
             archive_url = urlparse.urljoin(settings.DOMAIN, archive_url)
+
             tweet_text = "Results for %s: %s" % (current_question.question, archive_url)
+            if len(tweet_text) > 140:
+                tweet_text = "Results for yesterday's question: %s" % archive_url
+
             try:
                 status = twitter_api.PostUpdate(tweet_text)
                 self.stdout.write(status.text + "\n")
